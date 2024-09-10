3ccc6e712f11dc9d86bb384bbd72e794df496224
Noransaber <noransaber685@gmail.com>
2024-09-10 18:49:03 +0300

    Adding project files and folders

---
+++ app/page.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import ProjectSection from './components/ProjectSection';
+import EmailSection from './components/EmailSection';
+import Footer from './components/Footer';
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col bg-[#121212] '>
+      <NavBar />
+      <div className='container mt-24 mx-auto px-12 py-4'>
+        <HeroSection />
+        <AboutSection />
+        <ProjectSection />
+        <EmailSection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
